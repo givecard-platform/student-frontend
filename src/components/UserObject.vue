bc15d9146e4f82c925ae2fc4f286bc18bd43a1ea
leebpo <leethebookworm@gmail.com>
2022-10-06 13:37:25 -0400

    code runs in ReportingView but not imported

--- src/components/UserObject.vue
+++ src/components/UserObject.vue
@@ -1,39 +1,42 @@
-<!--Javascript objects, not sure where in the doc to put them-->
-function Date(day, month, year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
-}
-
-function User(name, surname, card_digits, exp_date, balance) = {
-    this.name = name;
-    this.surname = surname;
-    this.card_digits = card_digits;
-    this.exp_date = exp_date;
-    this.balance = balance;
-    this.transactions = [];
-    this.num_transactions = 0;
-
-    add_transaction = function(transaction) {
-        this.transactions = this.transactions.push(transaction);
-        this.num_transactions++;
-    }
-}
-
 <script>
+    // let function Transaction(vendor, amount, date) {
+    //     this.vendor = vendor,
+    //     this.amount = amount,
+    //     this.date = date,
+    // }
+    // let function Date(day, month, year) {
+    //     this.day = day,
+    //     this.month = month,
+    //     this.year = year,
+    // }
+
+    // let function User(name, surname, card_digits, exp_date, balance) {
+    //     this.name = name,
+    //     this.surname = surname,
+    //     this.card_digits = card_digits,
+    //     this.exp_date = exp_date,
+    //     this.balance = balance,
+    //     this.transactions = [],
+    //     this.num_transactions = 0,
+
+        // add_transaction = function(transaction) {
+        //     this.transactions = this.transactions.push(transaction);
+        //     this.num_transactions++;
+        // }
+    // }
     export default {
         data() {
-            return User("Hannah", "Kim", 4030, Date(25, 1, 2002), 233.21);
+            // return User("Hannah", "Kim", '4030', Date(25, 1, 2002), 233.21)
+        
+            return {
+                name: "Helen",
+                surname: "Kim",
+                card_digits: '4030',
+                exp_date: '25/01/2002',
+                balance: 233.21,
+            }
         }
     }
-</script>
 
-<template>
-<ul>
-    <li>
-        <span>{this.name}<span>
-        <span>{this.surname}<span>
-        <span>{this.card_digits}<span>
-        <span>{this.balance}<span>
-    <li>
-</template>
+
+</script>
