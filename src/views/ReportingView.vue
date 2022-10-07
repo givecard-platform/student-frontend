a71a820eca57755157b9c71154159dfe40f67980
leebpo <leethebookworm@gmail.com>
2022-10-07 00:17:58 -0400

    table not appearing

--- src/views/ReportingView.vue
+++ src/views/ReportingView.vue
@@ -3,21 +3,24 @@
     export default {
         data() {
             return {
-                name: "Helen",
+                name: "Hannah",
                 surname: "Kim",
                 card_digits: '4020',
-                exp_date: '26/01/2002',
+                exp_date: '01/26/2032',
                 balance: 233.21,
-                transactions: [{vendor: "Starbucks", amount: 4.99, date: '31/10/2022',}],
+                transactions: [
+                    {vendor: "Walgreens", amount: 4.99, date: '01/10/2022',},
+                    {vendor: "CVS", amount: 1.25, date: '01/15/2022'}
+                ],
                 num_transactions: 0,
             }
         },
-        methods : {
-            add_transaction({vendor: v, amount: a, date: d,}) {
-                this.transactions = this.transactions.push({vendor: v, amount: a, date: d,});
-                this.num_transactions++;
-            }
-        }
+        // methods : {
+        //     add_transaction({vendor: v, amount: a, date: d,}) {
+        //         this.transactions = this.transactions.push({vendor: v, amount: a, date: d,});
+        //         this.num_transactions++;
+        //     }
+        // }
     }
 </script>
 
@@ -38,9 +41,12 @@
                 <span>{{this.balance}}</span>
             </li>
             <li>
-                <span v-for="t in this.transactions">
-                    {{t.vendor}}
-                </span>
+                <div>
+                    <b-table :items="this.transactions"></b-table>
+                </div>
+                <ul v-for="t in this.transactions">
+                    <li>{{t.vendor}}</li>
+                </ul>
             </li>
         </ul> 
     </body>
