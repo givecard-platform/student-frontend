2c9b38d3a507848370916b98dea4f1a5540bcfea
leebpo <leethebookworm@gmail.com>
2022-10-17 06:49:26 -0400

    just displays basic user info

--- src/views/ReportingView.vue
+++ src/views/ReportingView.vue
@@ -1,68 +1,19 @@
-<script lang="ts">
-    // import UserObject from '../components/UserObject.vue'   
-    import TableLite from "vue3-table-lite/ts"
+<script>
+    import UserObject from '/Users/joannelee/Code/Givecard_Code/student-frontend/src/components/UserObject.vue'
+    import TableLite from "vue3-table-lite"
     export default {
         components: {
-            TableLite
+            TableLite,
+            UserObject
         },
-        data() {
-            return {
-                name: "Hannah",
-                surname: "Kim",
-                card_digits: '4020',
-                exp_date: '01/26/2032',
-                balance: 233.21,
-                transactions: [
-                    {vendor: "Walgreens", amount: 4.99, date: '01/10/2022',},
-                    {vendor: "CVS", amount: 1.25, date: '01/15/2022'}
-                ],
-                colNames: [
-                    {
-                    label: "Vendor",
-                    field: "vendor",
-                    width: "10%",
-                    headerStyles: {"background": "black", "color": "white"},
-                    columnStyles: {"background": "gray", "color": "white"},
-                    sortable: true,
-                    isKey: true,
-                    },
-                    {
-                    label: "Amount",
-                    field: "amount",
-                    width: "10%",
-                    headerStyles: {"background": "black", "color": "white"},
-                    columnStyles: {"background": "gray", "color": "white"},
-                    sortable: true,
-                    isKey: true,
-                    },
-                    {
-                    label: "Date",
-                    field: "date",
-                    width: "10%",
-                    headerStyles: {"background": "black", "color": "white"},
-                    columnStyles: {"background": "gray", "color": "white"},
-                    sortable: true,
-                    isKey: true,
-                    }
-                ],
-                num_transactions: 2
-            }
-        },
-        // methods : {
-        //     add_transaction({vendor: v, amount: a, date: d,}) {
-        //         this.transactions = this.transactions.push({vendor: v, amount: a, date: d,});
-        //         this.num_transactions++;
-        //     }
-        // }
     }
 </script>
 
 <template>
-    <div>
-    <h1 text-align="center">reporting page</h1>
-    <p>Name: {{name}} {{surname}}</p>
-    <p>Last 4 digits: {{card_digits}}</p>
-    <p>Balance: {{balance}}</p>
-    <table-lite style="color:green;" :columns="colNames" :rows="transactions" :total="num_transactions"></table-lite>
-    </div>
+    
+    <main>
+        <h1 text-align="center">Reporting Page</h1>
+        <UserObject />
+    </main>
+    
 </template>
