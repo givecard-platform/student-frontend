80aa59ab86d866177d1d888d2b2168558d878009
Joanne (Jo) Lee <70518354+leebpo@users.noreply.github.com>
2022-10-30 00:32:49 -0400

    added fetching async

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ReportingView from '../views/ReportingView.vue'
 import SpendingView from '../views/SpendingView.vue'
+import DataView from '../views/DataView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,7 +20,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/SpendingView.vue')
-    }
+    },
+    {
+      path: '/testing_async',
+      name: 'Testing',
+      component: DataView
+    },
   ]
 })
 
