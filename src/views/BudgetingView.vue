f96c025b2a456ffc7ce8b172d07f2dfb57855661
Morgan Seltzer <MorganSeltzer000@gmail.com>
2022-11-14 11:30:05 -0500

    Improved formatting for the budget

--- src/views/BudgetingView.vue
+++ src/views/BudgetingView.vue
@@ -11,7 +11,7 @@
             return {
                 goalList : ['Spend less this month than last month'], //will later fetch data
                 completedGoals : ["Save $10 this month"],
-                exampleList : ["Spend less this month than last month",
+                exampleGoals : ["Spend less this month than last month",
                 "Spend less money on something that you don't need, and instead do a different activity you enjoy that doesn't cost money.",
                 "Save $10 this month",
                 "Pay off $10 of debt. This makes it so you have to pay less total money on the debt",
@@ -25,6 +25,9 @@
                 noGoal : document.getElementById("newTaskName"), //easy way to do it, not sure if the best way
                 anyGoal : ""
             }
+        },
+        computed: {
+            randomGoal: function() {return this.exampleGoals[Math.floor(Math.random() * this.exampleGoals.length)]}
         },
         methods : {
             addGoal() {
@@ -50,22 +53,29 @@
 <template>
     <main>
         <h1 text-align="center">Budget Page</h1>
+
+        <div class="control"> <!-- This is needed to make sure the added goals don't resize the textarea, which was happening before -->
+        <h2 text-align="center">Add a Goal</h2>
         <div class="field has-addons">
-        <button class="button is-small is-responsive is-rounded" v-on:click="addGoal()">Add Goal</button>
-        <textarea class="textarea" type="text" placeholder="e.g. Hello world" v-model="anyGoal"></textarea>
+        <button class="button is-medium is-responsive is-rounded is-primary" v-on:click="addGoal()">Add Goal</button>
+        <textarea class="textarea has-background-dark has-text-white" type="text" v-model="anyGoal"></textarea>
         </div>
 
         <h2 text-align="center">Current Goals</h2>
         <div class="field has-addons" v-for="goal in goalList">
-            <button class="button is-small is-responsive is-rounded" v-on:click="completeGoal(goal)">Complete Goal</button>
-            <div class="box">{{goal}}</div>
-            <button class="button is-small is-responsive is-rounded" v-on:click="deleteGoal(goal, 'regular')">Delete</button>
+            <button class="button is-medium is-responsive is-rounded is-primary" v-on:click="completeGoal(goal)">Complete Goal</button>
+            <div class="box has-background-dark has-text-white">{{goal}}</div>
+            <button class="button is-medium is-responsive is-rounded is-danger" v-on:click="deleteGoal(goal, 'regular')">Delete</button>
         </div>
 
         <h2 text-align="center">Completed Goals</h2>
         <div class="field has-addons" v-for="goal in completedGoals">
-            <div class="box">{{goal}}</div>
-            <button class="button is-small is-responsive is-rounded" v-on:click="deleteGoal(goal, 'completed')">Delete</button>
+            <div class="box has-background-dark has-text-white">{{goal}}</div>
+            <button class="button is-medium is-responsive is-rounded is-danger" v-on:click="deleteGoal(goal, 'completed')">Delete</button>
+        </div>
+
+        <h2 text-align="center">Example Goals</h2>
+        <div class="box has-background-dark has-text-white">{{randomGoal}}</div>
         </div>
     </main>
 </template>
