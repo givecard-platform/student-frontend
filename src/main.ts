c209f552968f01dbedd6b9696ed2bd1be6929566
Joanne Lee <leebpo@bc.edu>
2022-11-29 02:47:38 -0500

    started on translation, not working

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,28 @@ import router from './router'
 
 import './assets/main.css'
 
+// import { createI18n } from 'vue-i18n'
+// import I18n from './i18n.js' //i18n.js supposed to be generated with npm install vue-i18n but it didn't
+//the one that exists now was manually made and the file contents were copied pasted from website
+// import VueI18n from "vue-i18n";
+
+// const i18n = createI18n({
+//     locale:
+//       localStorage.getItem('lang') ||
+//       // Detect user's browser language
+//       I18n.detectLanguage() ||
+//       process.env.VUE_APP_I18N_LOCALE,
+//     fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+//     // Load selected lang's .json file
+//     messages: I18n.loadLocaleMessages()
+//   })
+
 const app = createApp(App)
 
 app.use(router)
+// app.use(i18n)
 
 app.mount('#app')
+
+//I'm getting "-bash: vue-translation-manager: command not found" when I try to run 
+//vue-translation-manager translate (step )
